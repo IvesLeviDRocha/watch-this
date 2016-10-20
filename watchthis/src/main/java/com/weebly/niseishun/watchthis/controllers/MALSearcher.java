ce115f5c2fab8a0d1336dced10b8c19d8361053c
Levi R <ilevidr@gmail.com>
2016-10-20 00:53:56 -0300

    created method stubs with doc

--- watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/MALSearcher.java
+++ watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/MALSearcher.java
@@ -1,5 +1,34 @@
 package com.weebly.niseishun.watchthis.controllers;
 
+import java.util.ArrayList;
+
+import com.weebly.niseishun.watchthis.model.Entry;
+import com.weebly.niseishun.watchthis.model.User;
+
 public class MALSearcher {
+	
+	/**
+	 * get last users to update a series on MAL
+	 * @param url of series
+	 * @param numOfUsers to be returned
+	 * @return list of users 
+	 */
+	public ArrayList<User> getLastUpdatedUsers(String url, int numOfUsers) {
+		ArrayList<User> users = new ArrayList<User>();
+		//TODO
+		return users;
+	}
+	
+	/**
+	 * get the most liked series by given users
+	 * @param users whose lists should be checked
+	 * @param minLikes minimum number of users to have liked a series for it to be recommended
+	 * @return list of recommended series
+	 */
+	public ArrayList<Entry> getRecommendedSeriesFromUsers(ArrayList<User> users, int minLikes) {
+		ArrayList<Entry> entries = new ArrayList<Entry>();
+		//TODO
+		return entries;
+	}
 
 }
--- watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/QueryHandler.java
+++ watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/QueryHandler.java
@@ -7,6 +7,12 @@ import com.weebly.niseishun.watchthis.model.Source;
 
 public class QueryHandler {
 	
+	/**
+	 * run algorithm to find recommendations for a given series
+	 * @param url to series page
+	 * @param source of series page
+	 * @return list of recommendations
+	 */
 	public ArrayList<Entry> getRecommendationsWithURL(String url, Source source) {
 		ArrayList<Entry> recommendations = new ArrayList<Entry>();
 		//TODO
