b25e9691ca52269d65e675f0d0a31c96572e9808
Levi R <ilevidr@gmail.com>
2016-10-31 15:21:04 -0300

    Implemented QueryHandler

--- watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/QueryHandler.java
+++ watchthis/src/main/java/com/weebly/niseishun/watchthis/controllers/QueryHandler.java
@@ -1,22 +1,47 @@
 package com.weebly.niseishun.watchthis.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.weebly.niseishun.watchthis.model.Entry;
 import com.weebly.niseishun.watchthis.model.Source;
+import com.weebly.niseishun.watchthis.model.User;
 
+/**
+ * This handler performs the query algorithm to retrieve recommendations for a given input.
+ * 
+ * @author Levi Rocha
+ *
+ */
 public class QueryHandler {
-	
-	/**
-	 * run algorithm to find recommendations for a given series
-	 * @param url to series page
-	 * @param source of series page
-	 * @return list of recommendations
-	 */
-	public ArrayList<Entry> getRecommendationsWithURL(String url, Source source) {
-		ArrayList<Entry> recommendations = new ArrayList<Entry>();
-		//TODO
-		return recommendations;
-	}
+
+  /**
+   * run algorithm to find recommendations for a given series
+   * 
+   * @param url to series page
+   * @param source of series page
+   * @return list of recommendations
+   */
+  public ArrayList<Entry> getRecommendationsWithURL(String url, Source source) {
+    int numOfUsersToCheck = 100;
+    int minimumPopularity = 10;
+    ArrayList<Entry> recommendations = new ArrayList<Entry>();
+    switch (source) {
+      case MAL:
+        MALSearcher searcher = new MALSearcher();
+        ArrayList<User> users;
+        try {
+          users = searcher.getLastUpdatedUsers(url, numOfUsersToCheck);
+          recommendations = searcher.getRecommendedSeriesFromUsers(users, minimumPopularity, url);
+        } catch (IOException e) {
+          // TODO IOE error
+          e.printStackTrace();
+        }
+        break;
+      default:
+        // TODO invalid source error
+    }
+    return recommendations;
+  }
 
 }
