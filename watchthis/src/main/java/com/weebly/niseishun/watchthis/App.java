fe1adcbfde98233f74322085ebf71867ea1f735a
Levi R <ilevidr@gmail.com>
2016-11-03 14:23:47 -0300

    added comment to app

--- watchthis/src/main/java/com/weebly/niseishun/watchthis/App.java
+++ watchthis/src/main/java/com/weebly/niseishun/watchthis/App.java
@@ -6,9 +6,9 @@ import com.weebly.niseishun.watchthis.controllers.MainController;
 public class App {
   public static void main(String[] args) {
 
+    // create controller and launch program
     MainController controller = new MainController();
     controller.start();
-
 
   }
 }
